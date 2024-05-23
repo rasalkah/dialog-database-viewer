e752b5592546f739d3ef02e468a68e372f9ffbcc
rasalkah <pngn95@gmail.com>
2024-05-23 15:37:30 +0200

    Initial test

--- server/jest.config.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line no-undef
-module.exports = {
-  clearMocks: true,
-  preset: "ts-jest",
-  testEnvironment: "node",
-  modulePathIgnorePatterns: ["<rootDir>/dist/"],
-  setupFiles: ["dotenv/config"],
-};
